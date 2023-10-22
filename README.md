daad75108270f9447b27cf3e1e1f81912a5360f5
Olliv3r <oliveobom100@gmail.com>
2023-10-22 09:07:29 -0400

    Adicionada suporte de substituição de arquivo de origem pelo o traduzido

--- README.md
+++ README.md
@@ -13,12 +13,17 @@ cd Translator-Auto && pip install -r requirements.txt
 Exemplos:
 Traduz a legenda de um arquivo específico:
 ```
-./t-auto.py --source=en --target=pt --file subtitle/subtitle.srt
+./t-auto.py -s en -t pt -f subtitle/subtitle.srt
 ```
 Traduz a legenda de múltiplos arquivos do diretório padrão `subtitle`:
 ```
-./t-auto --source=en -target=pt --all
+./t-auto -s en -t pt -a
 ```
+Substituí o arquivo de origem pelo novo traduzido:
+```
+./t-auto -s auto -t pt -a -r
+```
+Obs!: *Antes de utilizar a opção `-r` ou `--replace-file` faça backup dos arquivos de legenda de origem, pois ao habilitar a substuição do arquivo de origem pelo traduzido através da opção `-r` ou `--replace-file`, poderá perder o arquivo antigo*.
 
 > [!NOTE]
 > Se queira traduzir vários arquivos de legenda `.srt` você precisa copiá-los para dentro do diretório `subtitle` o qual o programa usa para traduzir mais de um arquivo de legenda `.srt`. Se for apenas um arquivo não há necessidade de fazer esta etapa.
--- t-auto.py
+++ t-auto.py
@@ -7,9 +7,10 @@
 #
 # Por oliver, 27 de setembro 2023
 #
+#  Atualizado em 22 de outubro 2023
+#
 
 ### Módulos necessários:
-
 try:
     import deep_translator, requests
 except ModuleNotFoundError as err:
@@ -21,27 +22,25 @@ from os.path import (isdir)
 from src.language import languages
 from src.menu import banner
 
-# Versão do programa
-version = "0.0.6"
+### Versão do programa:
+version = "0.0.7"
 
-# Cores padrão
+### Cores padrão:
 w, r, g, y, b, d_g, c, m = "\033[0m", "\033[1;31m", "\033[1;32m", "\033[1;33m", "\033[1;34m", "\033[2;32m", "\033[1;35m", "\033[1;36m"
 
-# Referencia a classe
+### Faz referencia a classe:
 translate = deep_translator.GoogleTranslator()
 
-# Checa acesso a internet
+### Checa acesso a internet:
 def check_internet():
     print(f"\r{w}[{b}*{w}] {g}Checking internet access...{w}")
     try:
         requests.request('get', 'https://www.google.com', timeout=5)
         print(f"\r{w}[{b}+{w}] {g}The internet is connected!{w}")
     except requests.exceptions.ConnectionError:
-        print(f"\r{w}[{r}!{w}] {y}The internet is not connected!{w}")
-        exit()
+        exit(f"\r{w}[{r}!{w}] {y}The internet is not connected!{w}")
 
-### Traduz do idioma de origem 'source' para o 'target'
-
+### Traduz do idioma de origem 'source' para o 'target':
 def translate_text(source, target, text):
     translate.source = source
     translate.target = target
@@ -54,7 +53,6 @@ def translate_text(source, target, text):
         return subtitle_parts(source, target, text)
 
 ### Traduz o arquivo por partes caso a legenda ultrapasse 500 caracteres:
-
 def subtitle_parts(source, target, text):
     print(f"{w}[{y}!{w}] Subtitle too large detected. Translating in parts, this process takes a while. Please wait...{w}\n")
     
@@ -67,26 +65,13 @@ def subtitle_parts(source, target, text):
 
     return subtitle_parts_translated
     
-### Traduz apenas um arquivo:
-
-def translate_a_file(source, target, file, directory=""):
-    if directory != "":
-        file = f"subtitle/{file}"
-        
-    f = open(f"{file}")
-    f_text = f.read()
-    
-    print(f"{w}[{b}*{w}] Translating the subtitle, this process takes a little time. Please wait{w}...\n")
-    print(f"{w}[{b}*{w}] Translating from {m}{source} {w}to {m}{target}{w}...\n")
-    
-    translated = translate_text(source, target, f_text)
-    new_name_file = f.name.replace('.srt', f'-{target}.srt')
-    file_write(new_name_file, translated)
-
     
 ### Traduz todos os arquivos .srt disponíveis dentro do diretório subtitle/:
+def translate_all_files(source="", target="", directory="subtitle", replace_file=False):
 
-def translate_all_files(source, target, directory):
+    if not isdir(directory):
+        exit(f"{w}[{y}!{w}] The default directory was not found{w}")
+
     output = subprocess.Popen([f"ls {directory}"], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
     files = output.stdout.read().split("\n")
 
@@ -94,19 +79,43 @@ def translate_all_files(source, target, directory):
     
     for file in files:
         extension = file.split(".")[-1]
-        
+ 
         if extension == "srt":
             if(check_empty_file(f"{directory}/{file}")):
                 files_subtitle.append(file)
-            else:
-                 print(f"{w}[{y}!{w}] The subtitle file cannot be empty {m}{directory}/{file}{w}\n")
                  
     for file in files_subtitle:
-        translate_a_file(source, target, file, directory="subtitle")
+        translate_a_file(
+            source=source, 
+            target=target, 
+            file=f"{directory}/{file}",
+            replace_file=replace_file
+        )
 
+### Traduz apenas um arquivo:
+def translate_a_file(source="", target="", file="", replace_file=False):
+
+    if file.split('.')[-1] != "srt":
+        exit(f"{w}[{y}!{w}] .srt subtitle file is required.")
+        
+    if not check_empty_file(file):
+        exit(f"{w}[{y}!{w}] The subtitle file cannot be empty")
+    
+    f = open(f"{file}")
+    f_text = f.read()
+    
+    print(f"{w}[{b}*{w}] Translating the subtitle, this process takes a little time. Please wait{w}...\n")
+    print(f"{w}[{b}*{w}] Translating from {m}{source} {w}to {m}{target}{w}...\n")
+    
+    translated = translate_text(source, target, f_text)
+
+    if not replace_file:
+        new_name_file = f.name.replace('.srt', f'-{target}.srt')
+    else:
+        new_name_file = f.name
+    file_write(new_name_file, translated)
 
 ### Escreve um novo arquivo traduzido:
-
 def file_write(file, translated):
     
     if type(translated) == list:
@@ -123,11 +132,9 @@ def file_write(file, translated):
         f.write(translated)
         f.close()
         print(f'{w}[{g}√{w}] {g}Minimal translation completed successfully in {y}{f.name}{w}\n')
-    
-### Verifica se o arquivo estar vaziu:
 
+### Verifica se o arquivo estar vaziu:
 def check_empty_file(file):
-    
     file = open(file)
 
     if file.read() != "":
@@ -136,58 +143,64 @@ def check_empty_file(file):
         return False
 
 ### Opçôes:
-
 def options():
     parse = optparse.OptionParser()
     parse.description = "Traduz arquivos de legenda para qualquer idioma disponível"
-    parse.set_usage('./t-auto.py --source=<language> --target=<language> --file=<file>')
-    parse.add_option("--version", action="store_true", help="Versão do Program")
-    parse.add_option("--source", dest="source", help="Idioma de origem")
-    parse.add_option("--target", dest="target", help="Idioma alvo")
-    parse.add_option("--file", dest="file", help="Arquivo de legenda .srt")
-    parse.add_option("--all", action="store_true", help="Traduz todos os arquivos de legenda .srt disponíveis dentro do diretório padrão subtitle")
-    parse.add_option("--languages", action="store_true", help="Lista todos os idiomas")
+    parse.set_usage('./t-auto.py -s <source_lang> -t <target_lang> -f <file>')
+    parse.add_option("-v", "--version", action="store_true", help="Versão do Program")
+    parse.add_option("-s", "--source", dest="source", help="Idioma de origem")
+    parse.add_option("-t", "--target", dest="target", help="Idioma alvo")
+    parse.add_option("-f", "--file", dest="file", help="Arquivo de legenda .srt")
+    parse.add_option("-r", "--replace-file", help="Substitui o(s) arquivo(s) de origem por novo(s) traduzido(s)", action="store_true")
+    parse.add_option("-a", "--all", action="store_true", help="Traduz todos os arquivos de legenda .srt disponíveis dentro do diretório padrão subtitle")
+    parse.add_option("-l", "--languages", action="store_true", help="Lista todos os idiomas")
 
-    parse.version = version
-    
+    parse.version = version 
     options, args = parse.parse_args()
 
-    if options.source and options.target and options.file:
+    if options.source and \
+        options.target and \
+        options.file:
         if translate.is_language_supported(options.source) == False or \
         translate.is_language_supported(options.target) == False and \
         options.source != "auto":
-            print(f"{w}[{y}!{w}] Invalid language detected")
+            exit(f"{w}[{y}!{w}] Invalid language detected")
         else:
-            
-            if options.file.split('.')[-1] == "srt":
-
-                if (check_empty_file(options.file)):
-                    banner()
-                    check_internet()
-                    translate_a_file(options.source, options.target, options.file)
-                    print(f"{w}[{d_g}√{w}] Process completed successfully...{d_g}OK{w}")
-        
-                else:
-                    print(f"{w}[{y}!{w}] The subtitle file cannot be empty")
+            if options.replace_file:
+                replace_file=True
             else:
-                print(f"{w}[{y}!{w}] .srt subtitle file is required.")
-    
-    elif options.source and options.target and options.all:
+                replace_file=False
+
+            banner() ; check_internet()
+
+            translate_a_file(
+                source=options.source, 
+                target=options.target, 
+                file=options.file,
+                replace_file=replace_file
+            )
+     
+    elif options.source and \
+        options.target and \
+        options.all:
         if translate.is_language_supported(options.source) == False or \
         translate.is_language_supported(options.target) == False and \
         options.source != "auto":
-            print(f"{w}[{y}!{w}] Invalid language detected")
-
+            exit(f"{w}[{y}!{w}] Invalid language detected")
         else:
-            if isdir("subtitle"):
-                banner()
-                check_internet()
-                translate_all_files(options.source, options.target, directory="subtitle")
-                print(f"{w}[{d_g}√{w}] Process completed successfully...{d_g}OK{w}")
-            
+            if options.replace_file:
+                replace_file=True
             else:
-                exit(f"{w}[{y}!{w}] This directory does not exist: {y}subtitle{w}")
-    
+                replace_file=False
+
+            banner() ; check_internet()
+
+            translate_all_files(
+                source=options.source, 
+                target=options.target,
+                replace_file=replace_file
+            )
+   
     elif options.languages:
         print(f"{w}[{b}+{w}] All languages available:{w}")
         print(translate.get_supported_languages())
@@ -198,4 +211,5 @@ def options():
     else:
         print(f"{w}[{y}!{w}] Try using the options {d_g}-h{w},{d_g}--help{w}")
 
-options()
+if __name__ == '__main__':
+    options() 
